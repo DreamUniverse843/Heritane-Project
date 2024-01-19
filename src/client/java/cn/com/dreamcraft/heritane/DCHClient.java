5c6894960f25410b6ae07030c7aa94fb198a31b5
晓宇致梦Universe <53889721+DreamUniverse843@users.noreply.github.com>
2024-01-19 20:11:52 +0800

    Project Init

---
+++ src/client/java/cn/com/dreamcraft/heritane/DCHClient.java
@@ -0,0 +1,10 @@
+package cn.com.dreamcraft.heritane;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DCHClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/cn/com/dreamcraft/heritane/DCHDataGenerator.java
@@ -0,0 +1,11 @@
+package cn.com.dreamcraft.heritane;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DCHDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
